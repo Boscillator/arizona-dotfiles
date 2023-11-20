ba237a368250cc2dd1757532a4041385e793490e
Fred Buchanan <oscillator.b@gmail.com>
2023-11-20 07:13:09 -0500

    omnifunc and colorscheme tweaksn

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,3 +36,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', {})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
+
+-- Omnifunc
+vim.keymap.set('i', '<C-space>', '<C-x><C-o>', { desc = "autocomplete" })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,10 +7,15 @@ return {
     vim.cmd([[colorscheme valerie]])
     vim.o.background = 'dark'
 
-    -- Clear status bar hightlights
+    -- Clear gutter bar hightlights
     vim.cmd([[highlight clear SignColumn]])
+
+    -- Set gitsigns color
     vim.cmd([[highlight GitSignsAdd ctermfg=78 ctermbg=234]])
     vim.cmd([[highlight GitSignsDelete ctermfg=124 ctermbg=234]])
     vim.cmd([[highlight GitSignsChange ctermfg=78 ctermbg=234]])
+
+    -- Set popup color
+    vim.cmd([[highlight Pmenu ctermbg=219]])
   end
 }
